bdd6f9661c5c57d56537f409799e432986c75639
edward <L_zejie@163.com>
2018-08-02 17:42:57 +0800

    [add]添加并更新setup脚本

--- setup.py
+++ setup.py
@@ -10,22 +10,14 @@ from setuptools import setup, find_packages
 setup(
     name="features-extracter",
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description="text feature extracter",
     author="L_zejie",
     author_email='lzj_xuexi@163.com',
     url="https://github.com/Lzejie/features-extracter",
     license="MIT Licence",
-    # download_url='https://github.com/username/reponame/archive/0.1.tar.gz',
     keywords=['feature', 'extracter', 'nlp'],
     classifiers=[],
-    # entry_points={
-    #     'console_scripts': [
-    #         'command1 = advisorhelper.cmdline:execute'
-    #         'command2 = adviserserver.create_algorithm:run',
-    #         'command3 = adviserserver.run_algorithm:run'
-    #     ]
-    # },
     install_requires=[
         'sklearn',
         'gensim',
